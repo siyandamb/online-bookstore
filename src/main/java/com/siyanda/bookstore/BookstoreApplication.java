4f678bd0392d25b7af5f4e11226541b98115bcec
siyandamb <37158973+siyandamb@users.noreply.github.com>
2024-09-30 07:00:14 +0200

    Update BookstoreApplication.java

--- src/main/java/com/siyanda/bookstore/BookstoreApplication.java
+++ src/main/java/com/siyanda/bookstore/BookstoreApplication.java
@@ -3,7 +3,7 @@ package com.siyanda.bookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.siyanda.bookstore")
 public class BookstoreApplication {
 
     public static void main(String[] args) {
